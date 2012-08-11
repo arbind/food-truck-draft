8ccb4228b0611b6c256b17dfe9df1b2dd51bd28e
Arbind <arbind@rylyz.com>
2012-08-11 13:32:20 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FoodTruck::Application.config.secret_token = 'b6960ea1cc3a173d1ae2c596345355d86fcfde06fcc9e2c830068d7bae7ae63c9026834754f7f58284ace6ca20c132ca3f3ca7fb020d90603f96f15e8cb7f3c7'

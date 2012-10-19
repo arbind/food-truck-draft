e7d3d726648ebfa43d0d6587b503fb38734b9534
Arbind <arbind@rylyz.com>
2012-10-18 21:48:04 -0700

    adding autoexpire cache to geocoder

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,3 +1,30 @@
+# This class implements a cache with simple delegation to the Redis store, but
+# when it creates a key/value pair, it also sends an EXPIRE command with a TTL.
+# It should be fairly simple to do the same thing with Memcached.
+class AutoexpireCache
+  def initialize(store)
+    @store = store
+    @ttl = 86400
+  end
+
+  def [](url)
+    @store.[](url)
+  end
+
+  def []=(url, value)
+    @store.[]=(url, value)
+    @store.expire(url, @ttl)
+  end
+
+  def keys
+    @store.keys
+  end
+
+  def del(url)
+    @store.del(url)
+  end
+end
+
 Geocoder.configure do |config|
 
   # geocoding service (see below for supported options):
@@ -16,6 +43,7 @@ Geocoder.configure do |config|
 
   # caching (see below for details):
   # config.cache = REDIS
+  config.cache = AutoexpireCache.new(REDIS)
   config.cache_prefix = "geocoder:"
 end
 
